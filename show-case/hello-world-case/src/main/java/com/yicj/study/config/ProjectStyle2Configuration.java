2c36f04281da07e3de3ad51f1ff365da96a524db
yichengjie <626659321@qq.com>
2024-07-20 17:36:19 +0800

    jdk 版本降级到17

--- show-case/hello-world-case/src/main/java/com/yicj/study/config/ProjectStyle2Configuration.java
+++ show-case/hello-world-case/src/main/java/com/yicj/study/config/ProjectStyle2Configuration.java
@@ -1,41 +1,47 @@
-//package com.yicj.study.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-///**
-// * <p>
-// * ProjectStyle2Configuration
-// * </p>
-// *
-// * @author yicj
-// * @since 2024年07月13日 19:37
-// */
-//@Configuration
-//public class ProjectStyle2Configuration extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic() ;
-//        http.authorizeRequests()
-//            .anyRequest().authenticated() ;
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        var userDetailsService = new InMemoryUserDetailsManager() ;
-//        var user = User.withUsername("admin")
-//                .password("123")
-//                .authorities("read")
-//                .build();
-//        userDetailsService.createUser(user) ;
-//        //
-//        auth.userDetailsService(userDetailsService)
-//            .passwordEncoder(NoOpPasswordEncoder.getInstance());
-//    }
-//}
+package com.yicj.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * <p>
+ * ProjectStyle2Configuration
+ * </p>
+ *
+ * @author yicj
+ * @since 2024年07月13日 19:37
+ */
+@Configuration
+public class ProjectStyle2Configuration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(c -> {
+            c.realmName("my-basic-realm") ;
+            //c.authenticationEntryPoint(new MyBasicAuthenticationEntryPoint()) ;
+        })
+        .formLogin() ;
+        http.authorizeRequests()
+            .anyRequest().authenticated() ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService = new InMemoryUserDetailsManager() ;
+        var user = User.withUsername("admin")
+                .password("123")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user) ;
+        //
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        //
+        //auth.authenticationProvider(null) ;
+    }
+}
