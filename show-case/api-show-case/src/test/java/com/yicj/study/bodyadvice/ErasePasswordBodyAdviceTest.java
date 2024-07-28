559a24f498f2de2de6459f74652ed2bfc994d480
yichengjie <626659321@qq.com>
2024-07-28 17:50:25 +0800

    log init

---
+++ show-case/api-show-case/src/test/java/com/yicj/study/bodyadvice/ErasePasswordBodyAdviceTest.java
@@ -0,0 +1,81 @@
+package com.yicj.study.bodyadvice;
+
+
+import com.yicj.study.bodyadvice.anno.ErasePasswordAnno;
+import com.yicj.study.bodyadvice.anno.MapErasePasswordAnno;
+import com.yicj.study.controller.HelloController;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+/**
+ * <p>
+ * ErasePasswordBodyAdviceTest
+ * </p>
+ * @author yicj
+ * @since 2024年07月28日 15:34
+ */
+@Slf4j
+public class ErasePasswordBodyAdviceTest {
+
+    @Test
+    void findById() throws NoSuchMethodException {
+        // ResultEntity<BasicUser> findById(@PathVariable String id){
+        Method method = HelloController.class.getMethod("findById", String.class);
+        String value = getExtractorName(method);
+        log.info("value : {}", value);
+    }
+
+    @Test
+    void listForPage() throws NoSuchMethodException {
+        // ResultEntity listForPage()
+        Method method = HelloController.class.getMethod("findPage");
+        String value = getExtractorName(method);
+        log.info("value : {}", value);
+    }
+
+    @Test
+    void listForPageMono() throws NoSuchMethodException {
+        // ResultEntity listForPage()
+        Method method = HelloController.class.getMethod("findPage");
+        Mono<String> mono = getExtractorNameMono(method);
+        mono.subscribe(value -> log.info("value : {}", value));
+    }
+
+    private Mono<String> getExtractorNameMono(Method method){
+        return monoForErasePasswordAnno(method)
+                .switchIfEmpty(monoForMapErasePasswordAnno(method))
+                ;
+    }
+
+    private Mono<String> monoForErasePasswordAnno(Method method){
+        return Mono.just(method)
+                .filter(item -> item.isAnnotationPresent(ErasePasswordAnno.class))
+                .map(item -> item.getAnnotation(ErasePasswordAnno.class))
+                .map(ErasePasswordAnno::value)
+                .filter(StringUtils::isNotBlank);
+    }
+
+    private Mono<String> monoForMapErasePasswordAnno(Method method){
+        return  Mono.just(method).filter(item -> item.isAnnotationPresent(MapErasePasswordAnno.class))
+                .map(item -> item.getAnnotation(MapErasePasswordAnno.class))
+                .map(MapErasePasswordAnno::value)
+                .filter(StringUtils::isNotBlank) ;
+    }
+
+
+    private String getExtractorName(Method method){
+        return  Optional.ofNullable(method.getAnnotation(ErasePasswordAnno.class))
+                .map(ErasePasswordAnno::value)
+                .filter(StringUtils::isNotBlank)
+                .map(String::trim)
+                .orElseGet(() -> {
+                    MapErasePasswordAnno pageErasePasswordAnno = method.getAnnotation(MapErasePasswordAnno.class);
+                    return pageErasePasswordAnno != null ? pageErasePasswordAnno.value() : null;
+                });
+    }
+}
