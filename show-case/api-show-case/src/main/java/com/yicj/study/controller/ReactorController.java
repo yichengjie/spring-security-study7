7c2b581a6a587dc9fb6aefa31568d36301d960ab
yichengjie <626659321@qq.com>
2024-08-04 07:48:47 +0800

    reactor test2

--- show-case/api-show-case/src/main/java/com/yicj/study/controller/ReactorController.java
+++ show-case/api-show-case/src/main/java/com/yicj/study/controller/ReactorController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -29,6 +32,17 @@ public class ReactorController {
     @GetMapping("/reactor/hello")
     public Flux<String> hello(){
         return Flux.range(1,200)
+                .map(item -> helloService.slow(2) + item)
+                .doOnNext(value -> log.info("====> {}", value)) ;
+    }
+
+    @GetMapping("/reactor/hello2")
+    public Flux<String> hello2(){
+        List<Integer> list = new ArrayList<>() ;
+        for (int i = 0; i < 200; i++) {
+            list.add(i) ;
+        }
+        return Flux.fromIterable(list)
                 .map(item -> helloService.slow(2) + item)
                 .doOnNext(value -> log.info("====> {}", value)) ;
     }
