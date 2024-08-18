adafba0b39f603ddd45656960b1ac2bfa3166082
yichengjie <626659321@qq.com>
2024-08-18 19:56:24 +0800

    项目集成liquibase

--- show-case/jpa-show-case/src/main/java/com/yicj/study/jpa/config/AppConfig.java
+++ show-case/jpa-show-case/src/main/java/com/yicj/study/jpa/config/AppConfig.java
@@ -1,19 +1,6 @@
 package com.yicj.study.jpa.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 /**
  * <p>
--- show-case/jpa-show-case/src/main/java/com/yicj/study/jpa/config/DatasourceConfig.java
+++ show-case/jpa-show-case/src/main/java/com/yicj/study/jpa/config/DatasourceConfig.java
@@ -1,14 +1,10 @@
 package com.yicj.study.jpa.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
 
 /**
  * <p>
