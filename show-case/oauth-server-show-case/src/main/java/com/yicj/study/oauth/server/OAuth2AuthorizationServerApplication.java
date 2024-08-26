16698d8e66fea99be5d393895dac10694d3f994f
yichengjie <626659321@qq.com>
2024-08-26 17:00:12 +0800

    opt jpa test

---
+++ show-case/oauth-server-show-case/src/main/java/com/yicj/study/oauth/server/OAuth2AuthorizationServerApplication.java
@@ -0,0 +1,19 @@
+package com.yicj.study.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * OAuth2AuthorizationServerApplication
+ * </p>
+ *
+ * @author yicj
+ * @since 2024/08/26 16:46
+ */
+@SpringBootApplication
+public class OAuth2AuthorizationServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2AuthorizationServerApplication.class, args) ;
+    }
+}
