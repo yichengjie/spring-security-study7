35030fce94e12ee0f932cbda0d1971d8e2007550
yichengjie <626659321@qq.com>
2024-09-10 22:11:12 +0800

    query by example

--- show-case/jpa-show-case/src/main/java/com/yicj/study/jpa/repository/FeatureMessageRepository.java
+++ show-case/jpa-show-case/src/main/java/com/yicj/study/jpa/repository/FeatureMessageRepository.java
@@ -4,14 +4,10 @@ import com.yicj.study.jpa.entity.FeatureMessage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Meta;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 /**
@@ -24,7 +20,8 @@ import java.util.List;
  */
 @Repository
 public interface FeatureMessageRepository
-        extends JpaRepositoryImplementation<FeatureMessage, String> {
+        extends JpaRepositoryImplementation<FeatureMessage, String>
+        /*, QuerydslPredicateExecutor<FeatureMessage>*/ {
 
     @Query("select f from FeatureMessage f where f.status = ?1")
     List<FeatureMessage> listByStatus(Integer status, Sort sort) ;
