f6b7b0fe29f8856d7a336362650b9edc6d339966
yichengjie <626659321@qq.com>
2024-09-13 22:51:23 +0800

    gateway test

---
+++ show-case/gateway-show-case/src/main/java/com/yicj/study/gateway/HelloGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.yicj.study.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yicj
+ * @since 2024/9/13 22:43
+ */
+@SpringBootApplication
+public class HelloGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGatewayApplication.class, args);
+    }
+}
