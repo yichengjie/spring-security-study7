337e424d0a6185f80c399fda6660d97c4fc29b13
yichengjie <626659321@qq.com>
2024-09-24 23:02:58 +0800

    gateway test

--- show-case/security-show-case/src/main/java/com/yicj/study/security/HelloSecurityApplication.java
+++ show-case/security-show-case/src/main/java/com/yicj/study/security/HelloSecurityApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author yicj
  * @since 2024/08/31 20:31
  */
-// https://zhuanlan.zhihu.com/p/626276527
+// https://zhuanlan.zhihu.com/p/626276527 //DelegatingFilterProxyRegistrationBean#getFilter
 @SpringBootApplication
 public class HelloSecurityApplication {
     public static void main(String[] args) {
