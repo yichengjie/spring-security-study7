230399e7bc0d6c8bd340ed27284145161a0f8d89
yichengjie <626659321@qq.com>
2024-09-26 18:27:35 +0800

    add docs

--- show-case/client-show-case/src/main/java/com/yicj/study/ClientApplication.java
+++ show-case/client-show-case/src/main/java/com/yicj/study/ClientApplication.java
@@ -1,11 +1,57 @@
 package com.yicj.study;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Set;
 
+@Slf4j
 @SpringBootApplication
 public class ClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args) ;
+        //SpringApplication.run(ClientApplication.class, args) ;
+        System.exit(SpringApplication.exit(SpringApplication.run(ClientApplication.class, args)));
     }
+
+    @Bean
+    public ExitCodeGenerator exitCodeGenerator() {
+        return () -> 42;
+    }
+
+    @Component
+    static class MyBean{
+
+        public MyBean(ApplicationArguments args) {
+            boolean debug = args.containsOption("debug");
+            List<String> nonOptionArgs = args.getNonOptionArgs();
+
+            for (String each : nonOptionArgs) {
+                log.info("nonOptionArgs : {}", each);
+            }
+
+            // 所有参数
+            String[] sourceArgs = args.getSourceArgs();
+            for (String each : sourceArgs) {
+                log.info("sourceArgs : {}", each);
+            }
+            // 有key参数的key
+            Set<String> optionNames = args.getOptionNames();
+            for (String optionName : optionNames) {
+
+                // 有key参数的value
+                List<String> optionValues = args.getOptionValues(optionName);
+
+                for (String optionValue : optionValues) {
+                    log.info("optionName : {} , optionValue : {}", optionName, optionValue);
+                }
+            }
+            // if run with "--debug logfile.txt" prints ["logfile.txt"]
+        }
+    }
+
 }
