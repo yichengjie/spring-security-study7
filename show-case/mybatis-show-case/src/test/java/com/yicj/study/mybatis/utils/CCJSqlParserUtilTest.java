b3a2e8b47dde5069827f192a0571f72f48764d4c
yichengjie <626659321@qq.com>
2024-10-01 15:59:34 +0800

    project adjust

--- show-case/mybatis-show-case/src/test/java/com/yicj/study/mybatis/utils/CCJSqlParserUtilTest.java
+++ show-case/mybatis-show-case/src/test/java/com/yicj/study/mybatis/utils/CCJSqlParserUtilTest.java
@@ -8,7 +8,12 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectBody;
 import net.sf.jsqlparser.statement.select.WithItem;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -23,6 +28,7 @@ import java.util.List;
 @Slf4j
 public class CCJSqlParserUtilTest {
 
+    // https://blog.csdn.net/lsygood/article/details/138216759
     @Test
     void parse() throws Exception{
         String sql = "SELECT * FROM user WHERE status = 'active'";
@@ -33,34 +39,47 @@ public class CCJSqlParserUtilTest {
         System.out.println(select); // 输出：SELECT * FROM user WHERE status = 'inactive'
     }
 
-    @Test
-    void selectBody() throws JSQLParserException {
-        String SQL002 = "SELECT t1.a , t1.b  FROM tab1 AS t1 JOIN tab2 t2 ON t1.user_id  = t2.user_id";
-        Select select = (Select) CCJSqlParserUtil.parse(SQL002);
-        // select body
-        SelectBody selectBody = select.getSelectBody();
-        log.info("selectBody: {}", selectBody);
-        // with items
-        List<WithItem> withItemsList = select.getWithItemsList();
-        if (withItemsList != null) {
-            for (WithItem withItem : withItemsList) {
-                log.info("withItem: {}", withItem);
+    @Nested
+    class SelectBodyTest{
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+            "SELECT t1.a , t1.b  FROM tab1 AS t1 JOIN tab2 t2 ON t1.user_id  = t2.user_id",
+            "SELECT t1.a , t1.b  FROM tab1 AS t1, tab2 t2 WHERE t1.user_id = t2.user_id and t1.user_id = 1"
+        })
+        void selectBody(String sql) throws JSQLParserException {
+            Select select = (Select) CCJSqlParserUtil.parse(sql);
+            // select body
+            SelectBody selectBody = select.getSelectBody();
+            log.info("selectBody: {}", selectBody);
+            // with items
+            List<WithItem> withItemsList = select.getWithItemsList();
+            if (withItemsList != null) {
+                for (WithItem withItem : withItemsList) {
+                    log.info("withItem: {}", withItem);
+                }
             }
         }
     }
 
-    @Test
-    void withItems() throws JSQLParserException {
-        String SQL003="WITH t AS ( SELECT * FROM user WHERE user.user_name = 'test' ) SELECT t.* FROM t";
-        Select select = (Select) CCJSqlParserUtil.parse(SQL003);
-        //
-        SelectBody selectBody = select.getSelectBody();
-        log.info("selectBody: {}", selectBody);
-        // with items
-        List<WithItem> withItemsList = select.getWithItemsList();
-        for (WithItem withItem : withItemsList) {
-            log.info("withItem: {}", withItem);
-        }
 
+    @Nested
+    class WithItemsTest{
+        @Test
+        void withItems() throws JSQLParserException {
+            String SQL003="WITH t AS ( SELECT * FROM user WHERE user.user_name = 'test' ) SELECT t.* FROM t";
+            Select select = (Select) CCJSqlParserUtil.parse(SQL003);
+            //
+            SelectBody selectBody = select.getSelectBody();
+            log.info("selectBody: {}", selectBody);
+            // with items
+            List<WithItem> withItemsList = select.getWithItemsList();
+            for (WithItem withItem : withItemsList) {
+                log.info("withItem: {}", withItem);
+            }
+
+        }
     }
+
+
 }
